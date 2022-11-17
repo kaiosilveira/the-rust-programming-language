15b12febf040cbcfaada4acd35880494353396f4
Kaio Silveira <silveira.kaio@icloud.com>
2022-11-17 19:34:21 +0000

    chapter 11: example - ignoring tests

---
+++ chapter_11_automated_tests/examples/ignoring-tests/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // this test is too expensive to run in a regular basis or needs complex external services to be executed properly
+    }
+}
